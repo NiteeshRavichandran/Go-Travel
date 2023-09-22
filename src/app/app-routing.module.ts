5fcbee44ad800787374b2d3854e8001c40c7596e
NiteeshRavichandran <niteeshravichandran@gmail.com>
2023-09-22 16:50:32 +0530

    22/09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { SeatsComponent } from './seats/seats.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent},
   { path: 'admin', component: AdminComponent},
+  { path: 'seats', component: SeatsComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' }, 
   { path: '**', redirectTo: '/login' } 
 ];
--- src/app/seats/seats.component.ts
+++ src/app/seats/seats.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./seats.component.css']
 })
 export class SeatsComponent {
+  lowerSingleSeats = this.generateSeats(12, 'available');
+  singleUpperBerthSeats = this.generateSUBerth(6, 'available');
+  doubleLowerBerthSeats = this.generateDUBerth(6, 'available');
+  doubleUpperBerthSeats = this.generateDLBerth(6, 'available');
+
+  // Function to generate seat objects
+  private generateSeats(count: number, status: string): any[] {
+    const seats = [];
+    for (let i = 1; i <= count; i++) {
+      seats.push({ name: `LS${i}`, status: status });
+    }
+    return seats;
+  }
+  
+  private generateSUBerth(count: number, status: string): any[] {
+    const SUberth = [];
+    for (let i = 1; i <= count; i++) {
+      SUberth.push({ name: `SUB${i}`, status: status });
+    }
+    return SUberth;
+  }
+  private generateDUBerth(count: number, status: string): any[] {
+    const DUberth = [];
+    for (let i = 1; i <= count; i++) {
+      DUberth.push({ name: `DUB${i}`, status: status });
+    }
+    return DUberth;
+  }
+  private generateDLBerth(count: number, status: string): any[] {
+    const DLberth = [];
+    for (let i = 1; i <= count; i++) {
+      DLberth.push({ name: `DLB${i}`, status: status });
+    }
+    return DLberth;
+  }
+
+  // Function to toggle seat status
+  toggleSeatStatus(seat: any) {
+    if (seat.status === 'available' || seat.status === 'selected') {
+      seat.status = 'booked';
+    } else if (seat.status !== 'booked') {
+      seat.status = 'selected';
+    }
+  }
 
 }
